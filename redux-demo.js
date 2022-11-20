a23d3664ccb622057ad78b1ee321338ca5e4679b
sayedhany <sayed591999@gmail.com>
2022-11-20 19:57:53 +0200

    finsh intro test project

--- redux-demo.js
+++ redux-demo.js
@@ -1,1 +1,31 @@
+// import { legacy_createStore as createStore} from 'redux'
+// import { legacy_createStore as createStore } from "redux";
 const redux = require("redux");
+const counterReducer = (state = { counter: 0 }, action) => {
+  // console.log(action);
+  if (action.type === "increment") {
+    // console.log(state.counter);
+    return {
+      counter: state.counter + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+const store = redux.createStore(counterReducer);
+// console.log(store.getState());
+const counterSubscriper = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriper);
+store.dispatch({
+  type: "increment",
+});
+store.dispatch({
+  type: "decrement",
+});
